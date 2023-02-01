8b36373bd0c9685b9759accea28dbced2e1e2b34
Igor Danilov <danilovigor1999@gmail.com>
2023-02-02 00:52:27 +0300

    update:webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,10 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
-
+const path = require('path');
 
 module.exports = {
   entry: './src/app.ts',
-  devtool: 'inline-source-map',
   mode: 'development',
-  devServer: {
-    port: 80,
-    watchFiles: path.join(__dirname, 'src'),
-},
-  plugins: [new MiniCssExtractPlugin({
-    filename: '[name].css'
-  }),
-     new HtmlWebpackPlugin({
-       template: './src/index.html'
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: 'src/assets', to: 'assets'},
-        ],
-      })
-  ],
   module: {
     rules: [
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        include: [path.resolve(__dirname, "src")],
-      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -43,16 +13,10 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    extensionAlias: {
-     ".js": [".js", ".ts"],
-     ".cjs": [".cjs", ".cts"],
-     ".mjs": [".mjs", ".mts"]
-    }
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-    assetModuleFilename: 'assets/[name][ext]',
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
