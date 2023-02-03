26203a7ec6ccc1fbabad9afe4a64efa1a0e501d7
Neiman22 <pushkarev_av@inbox.ru>
2023-02-03 07:30:43 +0300

    feat: add addAddStockButton function in renderPage.ts

--- src/assets/renderPage.ts
+++ src/assets/renderPage.ts
@@ -1,18 +1,34 @@
 import {createElement} from './functions';
 import {moexTickerLast} from './api';
 
+moexTickerLast('GAZP').then(console.log);
+
 export function displaySecurityPage() {
     const securityPage = createElement('div', 'Ценные бумаги', 'security-page');
     document.body.appendChild(securityPage);
     const securityPageContainer = createElement('div', null, 'security-page__container');
     securityPage.appendChild(securityPageContainer);
 
-    const shareBlock = createElement('div', 'Акции', 'share-block');
-    securityPageContainer.appendChild(shareBlock);
+    const stockBlock = createElement('div', 'Акции', 'stock-block');
+    securityPageContainer.appendChild(stockBlock);
     const bondsBlock = createElement('div', 'Облигации', 'bonds-block');
     securityPageContainer.appendChild(bondsBlock);
     const etfBlock = createElement('div', 'БПИФ', 'etf-block');
     securityPageContainer.appendChild(etfBlock);
 
-    moexTickerLast('GAZP').then(console.log);
+    addAddStockButton(securityPageContainer);
+}
+
+function addAddStockButton(container: HTMLElement) {
+    const addStockButton = createElement('button', 'Add stock', 'add-stock-button');
+    container.appendChild(addStockButton);
+
+    addStockButton.addEventListener('click', () => {
+        addStocksBlock(container);
+    });
+}
+
+function addStocksBlock(container: HTMLElement) {
+    const stocksBlock = createElement('div', 'Stocks', 'stocks-block');
+    container.appendChild(stocksBlock);
 }
