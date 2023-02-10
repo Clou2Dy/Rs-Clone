087dce09deed248a8175cd553931359d711c517c
Neiman22 <pushkarev_av@inbox.ru>
2023-02-10 07:25:37 +0300

    fix: add renderTickers function in searchPage.ts

--- src/assets/api.ts
+++ src/assets/api.ts
@@ -19,24 +19,15 @@ export async function moexGetTickerPrice(ticker: string): Promise<number> {
   return price;
 }
 
-export async function getStocksTickers () {
-  const response = await fetch('https://iss.moex.com/iss/engines/stock/markets/shares/securities/.json');
-  const json = await response.json();
-  const data =  json.securities.data;
-  const tqbr = data.filter((elem: Array<string | number>) => elem[1] === 'TQBR')
-  return tqbr
+export async function getStocksTickers() {
+  try {
+    const response = await fetch('https://iss.moex.com/iss/engines/stock/markets/shares/securities/.json');
+    const json = await response.json();
+    const data =  json.securities.data;
+    const tqbr = data.filter((elem: Array<string | number>) => elem[1] === 'TQBR');
+    return tqbr;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
-
-export async function moexGetTickers(bord: string) {
-  const response = await fetch('https://iss.moex.com/iss/engines/stock/markets/shares/securities/.json').then(function(res) { return res.json()});
-  const data =  response.securities.data;
-  //const tickersName = extractTickersName(data, bord)
-  //return tickersName;
-}
-
-/*
-function extractTickersPrice(data: Array<Array<string | number>>, searchTerm: string): Array<[string, number, string]> {
-  return data.filter(subArray => (subArray[1] as string).toUpperCase() === searchTerm.toUpperCase())
-    .map(subArray => [subArray[0] as string, subArray[3] as number, subArray[2] as string]);
-}
-*/
--- src/assets/searchPage.ts
+++ src/assets/searchPage.ts
@@ -1,12 +1,15 @@
 import {createElement} from './functions'
+import {getStocksTickers} from './api'
 
-export function renderSearchPage(){
+export async function renderSearchPage(){
     const searchPage = createElement('div', null, 'search-page');
     document.body.appendChild(searchPage);
     const searchPageContainer = createElement('div', null, 'search-page__container');
     searchPage.appendChild(searchPageContainer);
     const searchBlock = createElement('div', null, 'search-block');
     searchPageContainer.appendChild(searchBlock);
+    const securitiesBlock = createElement('div', null, 'securities-block');
+    searchPageContainer.appendChild(securitiesBlock);
     const searchInput = createElement('input', null, 'search-input') as HTMLInputElement;
     searchBlock.appendChild(searchInput);
     searchInput.defaultValue = 'Компания, тикер, ISIN';
@@ -17,5 +20,51 @@ export function renderSearchPage(){
     cancelInput.addEventListener('click', () => {
         searchPage.style.display = 'none';
         securityPage.style.display = 'block';
+    });
+
+    const arrayStocksTickers = await getStocksTickers()
+    searchInput.addEventListener('click', () => {
+        searchInput.value = ''
+    })
+    searchInput.addEventListener('input', () => {
+        renderTickers(searchTickers(searchInput, arrayStocksTickers));
+    });
+}
+
+function searchTickers(inputElement: HTMLInputElement, array: Array<Array<string | number>>) {
+    const searchTerm = inputElement.value.toLowerCase();
+    const filteredTickers = array.filter((elem: Array<string | number>) => {
+      return ((elem[2] as string).toLowerCase().includes(searchTerm) ||
+              (elem[0] as string).toLowerCase().includes(searchTerm) ||
+              (elem[19] as string).toLowerCase().includes(searchTerm));
+    });
+    console.log(filteredTickers)
+    return filteredTickers;
+}
+
+function renderTickers(filteredStoks: Array<Array<string | number>>) {
+    const parentElement = document.querySelector('.securities-block');
+    while (parentElement?.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+    const headerStocks = createElement ('div', 'Акции', 'header-stocks')
+    parentElement?.appendChild(headerStocks);
+
+    filteredStoks.forEach((stock: Array<string | number>) => {
+        const stockElement = createElement('div', null, 'stock-info');
+
+        const stockName = createElement('div', `${stock[2]}`, 'stock-name');
+        stockElement.appendChild(stockName);
+
+        const stockTicker = createElement('div', `${stock[0]}`, 'stock-ticker');
+        stockElement.appendChild(stockTicker);
+
+        const stockLastPrice = createElement('div', `${stock[15]}`, 'stock-last');
+        stockElement.appendChild(stockLastPrice);
+
+        const stockPrevPrice = createElement('div', `${stock[3]}`, 'stock-prev');
+        stockElement.appendChild(stockPrevPrice);
+
+        parentElement?.appendChild(stockElement);
     });
 }
