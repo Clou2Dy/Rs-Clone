bfaa60284497b8fa3060ab6bc8c1deb7019030e6
DocSkullka <docskullka@gmail.com>
2023-02-13 23:49:52 +0300

    fix: fix date

--- src/data.ts
+++ src/data.ts
@@ -5,23 +5,20 @@ export default function data(){
     let day = new Date()
     let form = ('DD.MM.YYYY')
     let form1 = ('MMMM.YYYY')
-    document.querySelector('#month')?.addEventListener('click', ()=>{
-        document.querySelector('.interval')!.textContent = moment().date(0).format(form1)
+    document.querySelector('#month')!.addEventListener('click', ()=>{
+        document.querySelector('.interval')!.textContent = moment().month('string').format(form1)
     })
-    document.querySelector('#day')?.addEventListener('click', ()=>{
-        document.querySelector('.interval')!.textContent = 'Сегодня, ' + moment().year()
+    document.querySelector('#day')!.addEventListener('click', ()=>{
+        document.querySelector('.interval')!.textContent = 'Today, ' + moment().year()
     })
-    document.querySelector('#year')?.addEventListener('click', ()=>{
-        document.querySelector('.interval')!.textContent = day.toLocaleString('ru-US', year,)
+    document.querySelector('#year')!.addEventListener('click', ()=>{
+        document.querySelector('.interval')!.textContent = day.toLocaleString('ru-US', year)
     })
-    document.querySelector('#week')?.addEventListener('click', ()=>{
+    document.querySelector('#week')!.addEventListener('click', ()=>{
         let form = ('DD.MM.YYYY')
-        let str = moment().startOf('w').format(form).toString()
-        let end = moment().endOf('w').format(form).toString()
-        document.querySelector('.interval')!.textContent = str +'-'+end
-        
+        document.querySelector<any>('.interval')!.textContent = `${moment().day(1).format(form)} - ${moment().day(7).format(form)}`
     })
-    document.querySelector('.expensesDay')!.textContent ='Сегодня '  + moment().day(0).format(form)
-    document.querySelector('.expensesTom')!.textContent ='Вчера '  + moment().day(-1).format(form)
-    document.querySelector('.expensesLast')!.innerHTML = 'Позавчера ' + moment().day(-2).format(form)
+    document.querySelector('.expensesDay')!.textContent ='Today, '  + moment().day(1).format(form)
+    document.querySelector('.expensesTom')!.textContent ='Yesterday, '  + moment().day(0).format(form)
+    document.querySelector('.expensesLast')!.innerHTML = 'Day before yesterday, ' + moment().day(-1).format(form)
 }
