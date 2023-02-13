bace86f6d1836b49e1356d2ce11d9777cf4473d4
DocSkullka <docskullka@gmail.com>
2023-02-14 00:39:53 +0300

    fix:date

--- src/activeBtn.ts
+++ src/activeBtn.ts
@@ -10,25 +10,15 @@ export function butt(){
     
 }
 export function disabledButt (){
-    let butt:any = document.querySelector('.disa')
     let val:any = document.querySelector('.inpMoney')
-    butt.setAttribute('disabled', true)
-    butt.addEventListener('click', ()=>{
-        console.log( document.querySelector('.money'));
-        let suma = Number(val!.value)
-        let dox:any = Number(document.querySelector('.money')!.textContent) 
-        let mon =+ suma
-        // let mon = dox
-        console.log(suma);
-        console.log(dox);
-        document.querySelector('.money')!.textContent = mon.toString()
-        // console.log(mon);
-        
-        
+    document.querySelector<HTMLButtonElement>('.disa').addEventListener('click', ()=>{
+      
+  
     })
 }
 
-export function checkInp(){
+export function checkInp():void{
+   document.querySelector<HTMLButtonElement>('.disa').disabled = true 
    document.querySelector('.inpMoney').addEventListener('input',function(){
     if(document.querySelector<HTMLInputElement>('.inpMoney').value.length === 0){
         document.querySelector<HTMLButtonElement>('.disa').disabled = true 
--- src/data.ts
+++ src/data.ts
@@ -18,7 +18,8 @@ export default function data(){
         let form = ('DD.MM.YYYY')
         document.querySelector<any>('.interval')!.textContent = `${moment().day(1).format(form)} - ${moment().day(7).format(form)}`
     })
-    document.querySelector('.expensesDay')!.textContent ='Today, '  + moment().day(1).format(form)
-    document.querySelector('.expensesTom')!.textContent ='Yesterday, '  + moment().day(0).format(form)
-    document.querySelector('.expensesLast')!.innerHTML = 'Day before yesterday, ' + moment().day(-1).format(form)
+    let options = { weekday: 'long'};
+    document.querySelector('.expensesDay')!.textContent ='Today, '  + moment().subtract(0, 'days').format('DD.MM.YYYY'); 
+    document.querySelector('.expensesTom')!.textContent ='Yesterday, '  + moment().subtract(1, 'days').format('DD.MM.YYYY');
+    document.querySelector('.expensesLast')!.innerHTML = 'Day before yesterday, ' + moment().subtract(2, 'days').format('DD.MM.YYYY');
 }
