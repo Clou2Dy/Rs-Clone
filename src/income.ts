033f799074402274c2ff10ec757a94019fd92bc0
DocSkullka <docskullka@gmail.com>
2023-02-15 19:07:50 +0300

    fix: nothing

--- src/income.ts
+++ src/income.ts
@@ -1,9 +1,9 @@
 export default function income() {
-    document.querySelector('.paragraphIncome')?.addEventListener('click',()=>{
+    document.querySelector('.paragraphIncome')!.addEventListener('click',()=>{
         document.querySelector<HTMLElement>('.rashod')!.style.display = 'none'
         document.querySelector<HTMLElement>('.doxod')!.style.display = 'block'
     })
-    document.querySelector('.paragraphExpenses')?.addEventListener('click',()=>{
+    document.querySelector('.paragraphExpenses')!.addEventListener('click',()=>{
         document.querySelector<HTMLElement>('.rashod')!.style.display = 'block'
         document.querySelector<HTMLElement>('.doxod')!.style.display = 'none'
     })
