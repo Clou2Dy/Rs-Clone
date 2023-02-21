aa5dc5e4c12313bd53e4fe516ef1c0f5166b6bcd
Igor Danilov <danilovigor1999@gmail.com>
2023-02-21 22:46:45 +0300

    update: add img in block

--- src/addSpending.ts
+++ src/addSpending.ts
@@ -1,4 +1,6 @@
 import { butt, disabledButt, checkInp } from "./activeBtn"
+let img:string 
+let text:string
 export  function addSpending (){
     document.querySelector('.addExpenses')?.addEventListener('click', ()=>{
         document.querySelector<HTMLElement>('.changeMoney')!.style.display = 'block'
@@ -17,26 +19,43 @@ export  function addSpending (){
     disabledButt()
     checkInp()
 }
-
-
-export  function addUnd() {
+export  function addUnd():any {
     document.addEventListener('click',(el:any)=>{
         if (el.target.classList.contains('date') === true){
             document.getElementById('expensesDay')!.classList.remove('back')
             document.getElementById('expensesTom')!.classList.remove('back')
             document.getElementById('expensesLast')!.classList.remove('back')
             document.getElementById(el.target.id)!.classList.add('back')
-
+      
         }       
     })
-    document.addEventListener('click',(el:any)=>{ 
-       document.querySelectorAll('.backImg').forEach(element => {
-       element.classList.remove('backBlock')
-       })
-       if(el.target.classList.contains('backImg')){
-            el.target.classList.add('backBlock')
+    document.addEventListener('click',(el:any) =>{ 
+        if(el.target.classList.contains('backImg') || el.target.className === 'imgName' || 'imgBlock' === el.target.className){
+            // img = el.target.closest(".backImg").children[0].getAttribute('src')
+            // text = el.target.closest(".backImg").children[1].texContent
+            console.log(el.target.closest(".backImg").children[1]);
+        }
+        
+        document.querySelectorAll('.backImg').forEach(element => {
+            element.classList.remove('backBlock')
+        })
+        if(el.target.classList.contains('backImg')){
+            el.target.classList.add('backBlock') 
+            
        }else if(el.target.className === 'imgName' || 'imgBlock' === el.target.className){
-            el.target.parentElement.classList.add('backBlock')
-       }
-    })
+           el.target.parentElement.classList.add('backBlock')
+        }
+    }) 
 }
+export function block(){
+    let i = 0
+    document.querySelector('.disa')!.addEventListener('click', ()=>{
+        i++
+        const blockSpending = `<div class='blockContent'> <div class='imgDiv'><img src = ${img}></div></div> <div class='nameSpanding'></div> <div class='procent'></div> <div class='rasxoMoney'></div> </div>`
+        let block = document.createElement('div')
+        block.classList.add(`block${i}`, `block`)
+        block.innerHTML = blockSpending
+        document.querySelector('.spending')!.appendChild(block)
+    })  
+}
+
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import './style.scss';
 import data from './data';
-import block from './spending';
-import {addSpending, addUnd} from './addSpending';
+// import block from './spending';
+import {addSpending, addUnd, block} from './addSpending';
 import income from './income';
 import progress from './progressbar';
 
